eafe4fd8ad9f74e20baf9b16f2f35fd5244030cc
Andy Chen <andy27479@gmail.com>
2023-01-19 13:08:58 -0500

    added basic nltk sentiment

---
+++ pages/home.py
@@ -0,0 +1,176 @@
+import dash
+from dash import html, dash_table as dt
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+from api import Play, Apple
+from utils import *
+
+dash.register_page(__name__, path='/')
+
+search_bar = dbc.Row(
+    [
+        dbc.Col(
+            html.Abbr(
+                html.I(className="fa-solid fa-circle-info"),
+                title='''Search with Play Store app ID - play: <id>\
+                    \nSearch with App Store app title - apple: <title>\
+                    \nSearch with keywords - <keywords>'''
+            ),
+            width=1
+        ),
+        dbc.Col(
+            dbc.Input(id="search-term", type="search", placeholder="Enter an app ID or keywords", value="play: com.mojang.minecraftpe"),
+        ),
+        dbc.Col(
+            dbc.Button(
+                "Search", color="primary", class_name="ms-2",
+                id="search-button", n_clicks=0
+            ),
+            width="auto",
+        ),
+        html.Div(children="", id='search-temp', style={'display': 'none'}),
+        html.Div(children=[], id='result-temp', style={'display': 'none'})
+    ],
+    class_name="g-0 ms-auto flex-wrap mx-auto",
+    align="center",
+    style={'width': '600px'}
+)
+
+play_tab = dbc.Row(
+    [
+        dbc.Col(
+            [
+                html.Br(),
+                dbc.Spinner(
+                    [
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    html.Img(id='play-img', height="55px"), width="auto", style={"margin": "auto"}
+                                ),
+                                dbc.Col(
+                                    [
+                                        html.H2(html.A(id='play-title', target="_blank")),
+                                        html.H6(["App ID: ", html.Span(id='play-id')]),
+                                    ]
+                                )
+                            ]
+                        ),
+                        dt.DataTable(
+                            id='play-details',
+                            columns=[
+                                {"name": "Feature", "id": "Feature"},
+                                {"name": "Value", "id": "Value"}
+                            ],
+                            style_as_list_view=True,
+                            style_cell={'fontSize': 14, 'textAlign': 'left', 'paddingRight': '2rem'},
+                            style_header={'fontWeight': 'bold', 'backgroundColor': '#999999', 'color': 'white'},
+                            fill_width=False,
+                            cell_selectable=False
+                        )
+                    ]
+                )
+            ],
+            width=6
+        ),
+        dbc.Col(
+            dbc.Spinner(
+                dt.DataTable(
+                    id='play-reviews'
+                )
+            ),
+            width=6
+        )
+    ],
+    justify='center'
+)
+
+apple_tab = dbc.Row(
+    [
+        dbc.Col(
+            dbc.Spinner(
+                dt.DataTable(
+                    id='apple-details'
+                )
+            ),
+            width=6
+        ),
+        dbc.Col(
+            dbc.Spinner(
+                dt.DataTable(
+                    id='apple-reviews'
+                )
+            ),
+            width=6
+        )
+    ],
+    justify='center'
+) 
+
+
+layout = dbc.Container(
+    [
+        html.Br(),
+        search_bar,
+        html.Br(),
+        dbc.Tabs(
+            [
+                dbc.Tab(play_tab, label="Play Store", tab_id='play_tab', tab_style={'marginLeft':'auto'}),
+                dbc.Tab(apple_tab, label="Apple App Store", tab_id='apple_tab')
+            ],
+            active_tab="play_tab",
+        )
+    ]
+)
+
+
+# callback for updating search term when the search button is clicked
+@dash.callback(
+    Output('search-temp', 'children'),
+    [Input('search-button', 'n_clicks')],
+    [State('search-term', 'value')]
+)
+def update_term(click, term):
+    return term
+
+# callbacks for updating metadata
+@dash.callback(
+    [Output('result-temp', 'children')],
+    [Input('search-temp', 'children')]
+)
+def fetch_details(term):
+    if "play: " in term:
+        app_id = term.split("play:")[1].strip()
+        app = Play(app_id=app_id)
+        play_details = app.get_details()
+    elif "apple: " in term:
+        app_id = term.split("play:")[1].strip()
+    else:
+        app = Play(search=term)
+        play_details = app.get_details()
+    return [play_details]
+
+@dash.callback(
+    [
+        Output('play-title', 'children'),
+        Output('play-title', 'href'),
+        Output('play-id', 'children'),
+        Output('play-img', 'src'),
+    ],
+    [Input('result-temp', 'children')]
+)
+def update_meta(data):
+    return [data['title'], data['url'], data['appId'], data['icon']]
+
+@dash.callback(
+    [
+        Output('play-details', 'data'),
+        # Output('play-reviews', 'children'),
+        # Output('apple-details', 'children'),
+        # Output('apple-reviews', 'children')
+    ],
+    [Input('result-temp', 'children')]
+)
+def update_tables(data):
+    play_table = play_features(data)
+    return play_table
--- utils.py
+++ utils.py
@@ -1,17 +1,22 @@
+import string
+import re
 import pandas as pd
 import numpy as np
 from scipy.stats import skew
-import nltk
 from nltk import word_tokenize
 from nltk.corpus import stopwords
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 def play_features(data):
-    features = ['realInstalls', 'score', 'ratings', 'reviews', 'histogram', 'price', 'free', 'currency', 'sale',
+    features = ['realInstalls', 'score', 'description', 'ratings', 'reviews', 'histogram', 'price', 'free', 'currency', 'sale',
     'offersIAP', 'inAppProductPrice', 'genreId', 'contentRating', 'adSupported', 'containsAds', 'released']
     data_filtered = {k: data[k] for k in data.keys() & set(features)}
     data_filtered['reviewsStd'] = np.std(data_filtered['histogram'])
     data_filtered['reviewsSkew'] = skew(np.concatenate([np.full(n, i+1) for i, n in enumerate(data_filtered['histogram'])]))
+    sia = SentimentIntensityAnalyzer()
+    data_filtered['descriptionSentiment'] = sia.polarity_scores(process_text(data_filtered['description']))['compound']
     data_filtered.pop('histogram')
+    data_filtered.pop('description')
     df = pd.DataFrame(data_filtered, index=[0])
     df['releasedYear'] = pd.to_datetime(df['released']).dt.year
     df['IAPMin'] = df['inAppProductPrice'].str.extract(r'\$([^-]+)')
@@ -21,3 +26,10 @@ def play_features(data):
 
 def apple_features(data):
     pass
+
+def process_text(text):
+    stop = stopwords.words('english') + list(string.punctuation)
+    text_tokenized = word_tokenize(re.sub(r'\d+', '', text))
+    text_ls = [txt.lower() for txt in text_tokenized if txt.lower() not in stop]
+    processed_text = ' '.join(text_ls)
+    return processed_text
