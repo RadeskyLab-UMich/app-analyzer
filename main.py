d9db42edb3a3d42f58e5055c5a56ef5ef184584b
Andy Chen <andy27479@gmail.com>
2023-04-29 18:13:29 -0400

    added bcm back to main

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import dash
 from dash import DiskcacheManager, Dash, dcc, html, dash_table as dt
 import dash_bootstrap_components as dbc
 
-# import diskcache
-# cache = diskcache.Cache("./cache")
-# bcm = DiskcacheManager(cache)
+import diskcache
+cache = diskcache.Cache("./cache")
+bcm = DiskcacheManager(cache)
 
 THEME = dbc.themes.LUMEN
 
@@ -12,7 +12,7 @@ app = Dash(
     __name__,
     external_stylesheets=[THEME, "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"],
     use_pages=True, 
-    # background_callback_manager=bcm
+    background_callback_manager=bcm
 )
 server = app.server
 app.title = 'App Analyzer'
@@ -67,4 +67,4 @@ header = dbc.Navbar(
 app.layout = html.Div([header, dash.page_container])
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8080)
+    app.run_server(debug=True, port=8020)
