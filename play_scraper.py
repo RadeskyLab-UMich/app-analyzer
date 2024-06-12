a3628cca2f3ffd1fbce3a0da3a425fbc8dd54eb3
lindoncamaj <lindonc@umich.edu>
2024-06-12 15:57:27 -0400

    Add data collection section

--- play_scraper.py
+++ play_scraper.py
@@ -11,6 +11,66 @@ class GoogleApp:
         self.soup = BeautifulSoup(self.url.content, "html.parser", from_encoding='utf-8')
         self.soupprivacy = BeautifulSoup(self.urlprivacy.content, "html.parser", from_encoding='utf-8')
 
+    def get_app(self):
+        """
+        Function to get a dictionary of all variables in the Google Play Store scraper.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        dict of key value pairs for an app (ex: {collectsData: True})
+        """
+
+        app = {}
+        app["privacy"] = self.get_privacy_info()
+
+        if "doesn't collect or share any user data" not in app["privacy"][0]:
+            app["collectsData"] = True
+
+            for item in app["privacy"]:
+                if "Data shared" in item:
+                    app = self.get_privacy_info_shared(item, app)
+                if "Data collected" in item:
+                    app = self.get_privacy_info_collected(item, app)
+        else:
+            app["collectsData"] = False
+
+            app["data_shared_location"] = False
+            app["data_shared_personal_info"] = False
+            app["data_shared_financial_info"] = False
+            app["data_shared_health_fitness"] = False
+            app["data_shared_messages"] = False
+            app["data_shared_photos_videos"] = False
+            app["data_shared_audio_files"] = False
+            app["data_shared_files_docs"] = False
+            app["data_shared_calendar"] = False
+            app["data_shared_contacts"] = False
+            app["data_shared_app_activity"] = False
+            app["data_shared_browsing_history"] = False
+            app["data_shared_diagnostics"] = False
+            app["data_shared_identifiers"] = False
+
+            app["data_collected_location"] = False
+            app["data_collected_personal_info"] = False
+            app["data_collected_financial_info"] = False
+            app["data_collected_health_fitness"] = False
+            app["data_collected_messages"] = False
+            app["data_collected_photos_videos"] = False
+            app["data_collected_audio_files"] = False
+            app["data_collected_files_docs"] = False
+            app["data_collected_calendar"] = False
+            app["data_collected_contacts"] = False
+            app["data_collected_app_activity"] = False
+            app["data_collected_browsing_history"] = False
+            app["data_collected_diagnostics"] = False
+            app["data_collected_identifiers"] = False
+
+        return app
+
+
     def get_icon(self):
         data = self.soup.find("img", attrs={"class":"T75of QhHVZd"})
         if data:
@@ -19,29 +79,201 @@ class GoogleApp:
             data = self.soup.find("img", attrs={"class": "T75of nm4vBd arM4bb"})
             return data["src"]
 
-    def get_privacy(self):
+    def get_privacy_info(self):
+        """
+        Function to get the data collected information for an app
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        accum (list) - list of every category of data collected by the app and the specific types, if any.
+        """
+
+        accum = []
+
         data1 = self.soupprivacy.find("div", attrs={"class": "RGUlu"})
-        print(data1.get_text())
-        print()
+        accum.append(data1.get_text())
 
         data2 = self.soupprivacy.findAll("div", attrs={"class": "Mf2Txd"})
         for item in data2:
-            print(item.get_text(" | ", strip=True))
-            print()
+            accum.append(item.get_text(" | ", strip=True))
+
+        return accum
+
+    def get_privacy_info_collected(self, item, app):
+        if "Location" in item:
+            app["data_collected_location"] = True
+        else:
+            app["data_collected_location"] = False
+
+        if "Personal info" in item:
+            app["data_collected_personal_info"] = True
+        else:
+            app["data_collected_personal_info"] = False
+
+        if "Financial info" in item:
+            app["data_collected_financial_info"] = True
+        else:
+            app["data_collected_financial_info"] = False
+
+        if "Health and fitness" in item:
+            app["data_collected_health_fitness"] = True
+        else:
+            app["data_collected_health_fitness"] = False
+
+        if "Messages" in item:
+            app["data_collected_messages"] = True
+        else:
+            app["data_collected_messages"] = False
+
+        if "Photos and videos" in item:
+            app["data_collected_photos_videos"] = True
+        else:
+            app["data_collected_photos_videos"] = False
+
+        if "Audio files" in item:
+            app["data_collected_audio_files"] = True
+        else:
+            app["data_collected_audio_files"] = False
+
+        if "Files and docs" in item:
+            app["data_collected_files_docs"] = True
+        else:
+            app["data_collected_files_docs"] = False
+
+        if "Calendar" in item:
+            app["data_collected_calendar"] = True
+        else:
+            app["data_collected_calendar"] = False
+
+        if "Contacts" in item:
+            app["data_collected_contacts"] = True
+        else:
+            app["data_collected_contacts"] = False
+
+        if "App activity" in item:
+            app["data_collected_app_activity"] = True
+        else:
+            app["data_collected_app_activity"] = False
+
+        if "Web browsing" in item:
+            app["data_collected_browsing_history"] = True
+        else:
+            app["data_collected_browsing_history"] = False
+
+        if "App info and performance" in item:
+            app["data_collected_diagnostics"] = True
+        else:
+            app["data_collected_diagnostics"] = False
+
+        if "Device or other IDs" in item:
+            app["data_collected_identifiers"] = True
+        else:
+            app["data_collected_identifiers"] = False
+
+        return app
+
+    def get_privacy_info_shared(self, item, app):
+        if "Location" in item:
+            app["data_shared_location"] = True
+        else:
+            app["data_shared_location"] = False
+
+        if "Personal info" in item:
+            app["data_shared_personal_info"] = True
+        else:
+            app["data_shared_personal_info"] = False
+
+        if "Financial info" in item:
+            app["data_shared_financial_info"] = True
+        else:
+            app["data_shared_financial_info"] = False
+
+        if "Health and fitness" in item:
+            app["data_shared_health_fitness"] = True
+        else:
+            app["data_shared_health_fitness"] = False
+
+        if "Messages" in item:
+            app["data_shared_messages"] = True
+        else:
+            app["data_shared_messages"] = False
+
+        if "Photos and videos" in item:
+            app["data_shared_photos_videos"] = True
+        else:
+            app["data_shared_photos_videos"] = False
+
+        if "Audio files" in item:
+            app["data_shared_audio_files"] = True
+        else:
+            app["data_shared_audio_files"] = False
+
+        if "Files and docs" in item:
+            app["data_shared_files_docs"] = True
+        else:
+            app["data_shared_files_docs"] = False
+
+        if "Calendar" in item:
+            app["data_shared_calendar"] = True
+        else:
+            app["data_shared_calendar"] = False
+
+        if "Contacts" in item:
+            app["data_shared_contacts"] = True
+        else:
+            app["data_shared_contacts"] = False
+
+        if "App activity" in item:
+            app["data_shared_app_activity"] = True
+        else:
+            app["data_shared_app_activity"] = False
+
+        if "Web browsing" in item:
+            app["data_shared_browsing_history"] = True
+        else:
+            app["data_shared_browsing_history"] = False
+
+        if "App info and performance" in item:
+            app["data_shared_diagnostics"] = True
+        else:
+            app["data_shared_diagnostics"] = False
+
+        if "Device or other IDs" in item:
+            app["data_shared_identifiers"] = True
+        else:
+            app["data_shared_identifiers"] = False
+
+        return app
 
     def get_title(self):
+        """
+        Function to get the title of the app
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        str of app title (ex: Instagram)
+        """
+
         data = self.soup.find("h1", attrs={"class":"Fd93Bb F5UCq p5VxAd"})
         return data.get_text("\n" , strip=True).split("\n")[0]
 
 # test = GoogleApp("com.duolingo")
-# test.get_privacy()
+# print(test.get_app())
 
 # print()
 
 # test2 = GoogleApp("vitaminshoppe.consumerapp")
-# test2.get_privacy()
+# print(test2.get_app())
 
 # print()
 
 # test3 = GoogleApp("com.retroarch")
-# test3.get_privacy()
+# print(test3.get_app())
