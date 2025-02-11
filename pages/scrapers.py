9bdcf1a270dfc0f2017365cf83ac3500ed43753f
lindoncamaj <lindonc@umich.edu>
2025-02-11 17:31:08 -0500

    Add amazon scraper

--- pages/scrapers.py
+++ pages/scrapers.py
@@ -1,19 +1,20 @@
-import time
-import asyncio
 import dash
 from dash import html, dcc, dash_table as dt
 import dash_bootstrap_components as dbc
 import dash_mantine_components as dmc
 from dash.dependencies import Input, Output, State
-from api import Play, Apple
-from utils import *
-from youtube_scraper import *
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-import language_tool_python
-from apple_scraper import AppleApp
-from play_scraper import GoogleApp
-import io
+
+import time
 from datetime import datetime
+import asyncio
+import io
+
+from api import Play, Apple
+from apple_scraper import AppleApp
+from amazon_scraper import AmazonApp
+from play_scraper import GoogleApp
+from youtube_scraper import *
+
 
 dash.register_page(__name__)
 
@@ -25,30 +26,30 @@ features_tube = ['video_id', 'unavailable', 'private', 'requires_payment', 'is_l
             'removed', 'category', 'upload_date', 'publish_date', 'video_recommendations']
 
 
-# GOOGLE PLAY TAB CONFIGURATION
-play_tab = dbc.Container(
+
+# AMAZON TAB CONFIGURATION
+amazon_tab = dbc.Container(
     [
         html.Br(),
         dbc.Textarea(
-            className="mb-3", style={"height": "20rem"}, id="dl-input-play",
-            placeholder="Enter the app IDs for the app features you wish to download. Please place one app per line.\nEx:\ncom.duolingo\nvitaminshoppe.consumerapp"
+            className="mb-3", style={"height": "20rem"}, id="dl-input-amazon",
+            placeholder="Enter the app package names or ASINs for the app features you wish to download. Please place one app per line.\nEx:\nB07DXGT5C4\norg.pbskids.video\nB0C14XRMNM\ncom.learninga_z.onyourown"
         ),
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        dbc.Button("Download", color="primary", id="confirm-button-play", n_clicks=0),
-                        dcc.Download(id="dl-play")
+                    dbc.Button("Download", color="primary", id="confirm-button-amazon", n_clicks=0),
+                    dcc.Download(id="dl-amazon")
                     ],
                     width="auto"
                 ),
                 dbc.Col(
-                    dbc.Progress(id="dl-play-progress", style={"height": "2rem"}, animated=True, striped=True),
+                    dbc.Progress(id="dl-amazon-progress", style={"height": "2rem"}, animated=True, striped=True),
                     width=3
                 ),
             ],
-            class_name="g-2",
-            align='center'
+            class_name="g-1"
         ),
     ]
 )
@@ -76,6 +77,34 @@ apple_tab = dbc.Container(
                 ),
             ],
             class_name="g-1"
+        ),
+    ]
+)
+
+# GOOGLE PLAY TAB CONFIGURATION
+play_tab = dbc.Container(
+    [
+        html.Br(),
+        dbc.Textarea(
+            className="mb-3", style={"height": "20rem"}, id="dl-input-play",
+            placeholder="Enter the app IDs for the app features you wish to download. Please place one app per line.\nEx:\ncom.duolingo\nvitaminshoppe.consumerapp"
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Button("Download", color="primary", id="confirm-button-play", n_clicks=0),
+                        dcc.Download(id="dl-play")
+                    ],
+                    width="auto"
+                ),
+                dbc.Col(
+                    dbc.Progress(id="dl-play-progress", style={"height": "2rem"}, animated=True, striped=True),
+                    width=3
+                ),
+            ],
+            class_name="g-2",
+            align='center'
         ),
     ]
 )
@@ -122,9 +151,10 @@ layout = dbc.Container(
         html.Br(),
         dbc.Tabs(
             [
-                dbc.Tab(apple_tab, label="Apple", tab_id='apple_tab', tab_style={'marginLeft':'auto'}),
+                dbc.Tab(amazon_tab, label="Amazon", tab_id="amazon_tab", tab_style={'marginLeft':'auto'}),
+                dbc.Tab(apple_tab, label="Apple", tab_id='apple_tab'),
                 dbc.Tab(play_tab, label="Google Play", tab_id='play_tab'),
-                dbc.Tab(you_tab, label="YouTube", tab_id="you_tab")
+                dbc.Tab(you_tab, label="YouTube", tab_id="you_tab"),
             ],
             active_tab="play_tab",
         )
@@ -137,180 +167,125 @@ layout = dbc.Container(
 
 
 
-
-
-########################################################################
-# GOOGLE PLAY SCRAPER FUNCTIONS
-@dash.callback(
-    Output('dl-play', 'data'),
-    [
-        Input('confirm-button-play', 'n_clicks'),
-    ],
-    [
-        State('dl-input-play', 'value'),
-    ],
-    running=[
-        (Output("dl-play-progress", "animated"), True, False),
-    ],
-    progress=[
-        Output("dl-play-progress", "value"),
-        Output("dl-play-progress", "label"),
-        Output("dl-play-progress", "max")
-    ],
-    prevent_initial_call=True,
-    background=True
-)
-def update_play_info(set_progress, click, apps):
+def get_excel(df, df2, filename):
     """
-    Function to scrape data on Google Play apps and download them in an .xlsx file.
+    Function to turn dataframes into an excel workbook
 
     Parameters
     ----------
-    apps (str) - string of video IDs
+    df - Pandas Dataframe
+    df2 - Pandas dataframe
+    filename (str) - filename to give to excel file
 
     Returns
     -------
-    df (download) - dataframe of the scraped app ID information
+    Download of the excel file
     """
-
-    full_play_ls = []
-    not_found = []
-    not_found2 = []
-    apps_ls = apps.split('\n')
-    n_apps = len(apps_ls)
-
-
-    for idx, app_id in enumerate(apps_ls):
-        print(f"Fetching {idx + 1}/{n_apps}: {app_id}")
-        set_progress((idx + 1, f"{int((idx + 1) / n_apps * 100)} %", n_apps))
-        app_found = False
-        try:
-            app_info = Play(app_id=app_id.strip())
-            play_info = app_info.get_details()
-            time.sleep(0.5)
-            app_found = True
-        except Exception as e:
-            print(e)
-            not_found.append(app_id)
-
-        if app_found:
-            check = False
-            count = 0
-
-            while check == False and count < 30:
-                try:
-                    app = GoogleApp(app_id)
-                    play_info2 = app.get_app()
-
-                    if play_info2:
-                        play_info.update(play_info2)
-                        full_play_ls.append(play_info)
-                        check = True
-                except Exception as e:
-                    print(e)
-
-                count += 1
-                time.sleep(1)
-
-            if count == 30:
-                full_play_ls.append(play_info)
-
-        time.sleep(0.5)
-
-    for app_id in not_found:
-        app_found = False
-        try:
-            app_info = Play(app_id=app_id.strip())
-            play_info = app_info.get_details()
-            time.sleep(0.5)
-            app_found = True
-        except Exception as e:
-            print(e)
-            not_found2.append(app_id)
-
-        if app_found:
-            check = False
-            count = 0
-
-            while check == False and count < 30:
-                try:
-                    app = GoogleApp(app_id)
-                    play_info2 = app.get_app()
-
-                    if play_info2:
-                        play_info.update(play_info2)
-                        full_play_ls.append(play_info)
-                        check = True
-                except Exception as e:
-                    print(e)
-
-                count += 1
-                time.sleep(1)
-
-            if count == 30:
-                full_play_ls.append(play_info)
-
-        time.sleep(0.5)
-
-
-    df = pd.DataFrame(full_play_ls)
-    if "icon" in df:
-        df.drop(columns=["icon"], inplace=True)
-    if "headerImage" in df:
-        df.drop(columns=["headerImage"], inplace=True)
-    if "screenshots" in df:
-        df.drop(columns=["screenshots"], inplace=True)
-    if "video" in df:
-        df.drop(columns=["video"], inplace=True)
-    if "videoImage" in df:
-        df.drop(columns=["videoImage"], inplace=True)
-    if "descriptionHTML" in df:
-        df.drop(columns=["descriptionHTML"], inplace=True)
-    if "developerWebsite" in df:
-        df.drop(columns=["developerWebsite"], inplace=True)
-    if "privacyPolicy" in df:
-        df.drop(columns=["privacyPolicy"], inplace=True)
-
-    today = datetime.now()
-    formatted_date = today.strftime("%m/%d/%Y")
-    df["date_scraped"] = formatted_date
-
-    first_column = df.pop('date_scraped')
-    df.insert(0, 'date_scraped', first_column)
-
-
-    df2 = pd.DataFrame({"appId": not_found2})
-
-    # Using BytesIO to write to Excel format in memory
     with io.BytesIO() as buffer:
         with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
             df.to_excel(writer, sheet_name='apps_found', index=False)
             df2.to_excel(writer, sheet_name='not_found', index=False)
 
-            # Access the xlsxwriter workbook and worksheet objects
             workbook  = writer.book
             worksheet1 = writer.sheets['apps_found']
             worksheet2 = writer.sheets['not_found']
 
-            # Define a format for wrapped text.
             wrap_format = workbook.add_format({'text_wrap': True})
 
-            # Define a format to specify row height (e.g., 20)
             row_format = workbook.add_format()
-            row_format.set_text_wrap(True)  # If you also want to wrap text
-            row_height = 100  # Adjust the height as necessary
+            row_format.set_text_wrap(True)
+            row_height = 100
 
-            # Set text wrap format and row height for non-header rows in both sheets
-            for row_num in range(1, len(df) + 1):  # Start from 1 to skip the header
+            for row_num in range(1, len(df) + 1):
                 worksheet1.set_row(row_num, row_height, wrap_format)
 
-
-
-        # Important: You must seek back to the beginning of the buffer after writing
         buffer.seek(0)
-        return dcc.send_bytes(buffer.getvalue(), "play_features.xlsx")
+        return dcc.send_bytes(buffer.getvalue(), filename)
 
 
+
+
+
+
+########################################################################
+# AMAZON SCRAPER FUNCTIONS
+@dash.callback(
+    Output('dl-amazon', 'data'),
+    [
+        Input('confirm-button-amazon', 'n_clicks'),
+    ],
+    [
+        State('dl-input-amazon', 'value'),
+    ],
+    running=[
+        (Output("dl-amazon-progress", "animated"), True, False),
+    ],
+    progress=[
+        Output("dl-amazon-progress", "value"),
+        Output("dl-amazon-progress", "label"),
+        Output("dl-amazon-progress", "max")
+    ],
+    prevent_initial_call=True,
+    background=True
+)
+def update_amazon_info(set_progress, click, apps):
+    """
+    Function to scrape data on Apple iTunes store items such as apps and download them in an .xlsx file.
+
+    Parameters
+    ----------
+    apps (str) - string of app/song/etc. IDs
+
+    Returns
+    -------
+    df (download) - dataframe of the scraped ID information
+    """
+
+    full_amazon_ls = []
+    not_found = []
+    not_found2 = []
+    apps_ls = apps.split('\n')
+    n_apps = len(apps_ls)
+
+    for idx, app_id in enumerate(apps_ls):
+        print(f"Fetching {idx + 1}/{n_apps}: {app_id}")
+        set_progress((idx + 1, f"{int((idx + 1) / n_apps * 100)} %", n_apps))
+
+        try:
+            app_info = AmazonApp(id=app_id.strip())
+            amazon_details = app_info.get_app()
+            time.sleep(60)
+
+            if amazon_details == {}:
+                not_found.append(app_id)
+            else:
+                full_amazon_ls.append(amazon_details)
+        except Exception as e:
+            print(e)
+            not_found.append(app_id)
+
+        time.sleep(60)
+
+    for app_id in not_found:
+        try:
+            app_info = AmazonApp(id=app_id.strip())
+            amazon_details = app_info.get_app()
+            time.sleep(60)
+
+            if amazon_details == {}:
+                not_found2.append(app_id)
+            else:
+                full_amazon_ls.append(amazon_details)
+        except Exception as e:
+            print(e)
+            not_found2.append(app_id)
+
+        time.sleep(60)
+
+    df = pd.DataFrame(full_amazon_ls)
+    df2 = pd.DataFrame({"appId": not_found2})
+
+    return get_excel(df, df2, "amazon_features.xlsx")
 
 
 
@@ -463,34 +438,155 @@ def update_apple_info(set_progress, click, apps):
 
     df2 = pd.DataFrame({"appId": not_found2})
 
-    # Using BytesIO to write to Excel format in memory
-    with io.BytesIO() as buffer:
-        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-            df.to_excel(writer, sheet_name='apps_found', index=False)
-            df2.to_excel(writer, sheet_name='not_found', index=False)
-
-            # Access the xlsxwriter workbook and worksheet objects
-            workbook  = writer.book
-            worksheet1 = writer.sheets['apps_found']
-            worksheet2 = writer.sheets['not_found']
-
-            # Define a format for wrapped text.
-            wrap_format = workbook.add_format({'text_wrap': True})
-
-            # Define a format to specify row height (e.g., 20)
-            row_format = workbook.add_format()
-            row_format.set_text_wrap(True)  # If you also want to wrap text
-            row_height = 100  # Adjust the height as necessary
-
-            # Set text wrap format and row height for non-header rows in both sheets
-            for row_num in range(1, len(df) + 1):  # Start from 1 to skip the header
-                worksheet1.set_row(row_num, row_height, wrap_format)
+    return get_excel(df, df2, "apple_features.xlsx")
 
 
 
-        # Important: You must seek back to the beginning of the buffer after writing
-        buffer.seek(0)
-        return dcc.send_bytes(buffer.getvalue(), "apple_features.xlsx")
+
+
+########################################################################
+# GOOGLE PLAY SCRAPER FUNCTIONS
+@dash.callback(
+    Output('dl-play', 'data'),
+    [
+        Input('confirm-button-play', 'n_clicks'),
+    ],
+    [
+        State('dl-input-play', 'value'),
+    ],
+    running=[
+        (Output("dl-play-progress", "animated"), True, False),
+    ],
+    progress=[
+        Output("dl-play-progress", "value"),
+        Output("dl-play-progress", "label"),
+        Output("dl-play-progress", "max")
+    ],
+    prevent_initial_call=True,
+    background=True
+)
+def update_play_info(set_progress, click, apps):
+    """
+    Function to scrape data on Google Play apps and download them in an .xlsx file.
+
+    Parameters
+    ----------
+    apps (str) - string of video IDs
+
+    Returns
+    -------
+    df (download) - dataframe of the scraped app ID information
+    """
+
+    full_play_ls = []
+    not_found = []
+    not_found2 = []
+    apps_ls = apps.split('\n')
+    n_apps = len(apps_ls)
+
+
+    for idx, app_id in enumerate(apps_ls):
+        print(f"Fetching {idx + 1}/{n_apps}: {app_id}")
+        set_progress((idx + 1, f"{int((idx + 1) / n_apps * 100)} %", n_apps))
+        app_found = False
+        try:
+            app_info = Play(app_id=app_id.strip())
+            play_info = app_info.get_details()
+            time.sleep(0.5)
+            app_found = True
+        except Exception as e:
+            print(e)
+            not_found.append(app_id)
+
+        if app_found:
+            check = False
+            count = 0
+
+            while check == False and count < 30:
+                try:
+                    app = GoogleApp(app_id)
+                    play_info2 = app.get_app()
+
+                    if play_info2:
+                        play_info.update(play_info2)
+                        full_play_ls.append(play_info)
+                        check = True
+                except Exception as e:
+                    print(e)
+
+                count += 1
+                time.sleep(1)
+
+            if count == 30:
+                full_play_ls.append(play_info)
+
+        time.sleep(0.5)
+
+    for app_id in not_found:
+        app_found = False
+        try:
+            app_info = Play(app_id=app_id.strip())
+            play_info = app_info.get_details()
+            time.sleep(0.5)
+            app_found = True
+        except Exception as e:
+            print(e)
+            not_found2.append(app_id)
+
+        if app_found:
+            check = False
+            count = 0
+
+            while check == False and count < 30:
+                try:
+                    app = GoogleApp(app_id)
+                    play_info2 = app.get_app()
+
+                    if play_info2:
+                        play_info.update(play_info2)
+                        full_play_ls.append(play_info)
+                        check = True
+                except Exception as e:
+                    print(e)
+
+                count += 1
+                time.sleep(1)
+
+            if count == 30:
+                full_play_ls.append(play_info)
+
+        time.sleep(0.5)
+
+
+    df = pd.DataFrame(full_play_ls)
+    if "icon" in df:
+        df.drop(columns=["icon"], inplace=True)
+    if "headerImage" in df:
+        df.drop(columns=["headerImage"], inplace=True)
+    if "screenshots" in df:
+        df.drop(columns=["screenshots"], inplace=True)
+    if "video" in df:
+        df.drop(columns=["video"], inplace=True)
+    if "videoImage" in df:
+        df.drop(columns=["videoImage"], inplace=True)
+    if "descriptionHTML" in df:
+        df.drop(columns=["descriptionHTML"], inplace=True)
+    if "developerWebsite" in df:
+        df.drop(columns=["developerWebsite"], inplace=True)
+    if "privacyPolicy" in df:
+        df.drop(columns=["privacyPolicy"], inplace=True)
+
+    today = datetime.now()
+    formatted_date = today.strftime("%m/%d/%Y")
+    df["date_scraped"] = formatted_date
+
+    first_column = df.pop('date_scraped')
+    df.insert(0, 'date_scraped', first_column)
+
+
+    df2 = pd.DataFrame({"appId": not_found2})
+
+    return get_excel(df, df2, "play_features.xlsx")
 
 
 
